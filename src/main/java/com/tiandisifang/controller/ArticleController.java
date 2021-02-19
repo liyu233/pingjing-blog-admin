111df81ba1a0b84431174810890cf91ccf4f2019
liyu <liyu@zjex.com.cn>
2021-02-19 17:19:44 +0800

    init

--- src/main/java/com/tiandisifang/controller/ArticleController.java
+++ src/main/java/com/tiandisifang/controller/ArticleController.java
@@ -4,6 +4,7 @@ import com.tiandisifang.model.Article;
 import com.tiandisifang.service.ArticleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 
 @Controller
+@CrossOrigin
 @RequestMapping("article")
 public class ArticleController {
     @Autowired
