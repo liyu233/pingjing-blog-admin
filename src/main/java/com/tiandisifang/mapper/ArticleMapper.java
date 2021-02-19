b7fdcc897ac07231a5a31693c441b64a0d11b9ff
liyu <liyu@zjex.com.cn>
2021-02-19 15:03:10 +0800

    init

--- src/main/java/com/tiandisifang/mapper/ArticleMapper.java
+++ src/main/java/com/tiandisifang/mapper/ArticleMapper.java
@@ -2,10 +2,11 @@ package com.tiandisifang.mapper;
 
 import com.tiandisifang.model.Article;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Mapper
+@Repository
 public interface ArticleMapper {
     List<Article> getArticle();
 }
