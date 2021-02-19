7a21e69e1fbc0bdd9d196cf8f0890d73264b442e
liyu <liyu@zjex.com.cn>
2021-02-19 13:42:24 +0800

    init

--- src/main/java/com/tiandisifang/controller/ArticleController.java
+++ src/main/java/com/tiandisifang/controller/ArticleController.java
@@ -1,5 +1,25 @@
 package com.tiandisifang.controller;
 
-public class ArticleController {
+import com.tiandisifang.model.Article;
+import com.tiandisifang.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
+@Controller
+@RequestMapping("article")
+public class ArticleController {
+    @Autowired
+    private ArticleService articleService;
+
+    @ResponseBody
+    @PostMapping("list")
+    public List<Article> getList(Integer article_type){
+        System.out.println(article_type);
+        return articleService.getArticle();
+    }
 }
--- src/main/java/com/tiandisifang/model/Article.java
+++ src/main/java/com/tiandisifang/model/Article.java
@@ -1,5 +1,10 @@
 package com.tiandisifang.model;
+import lombok.Data;
 
+@Data
 public class Article {
-
+    private Integer id;
+    private StringBuilder articleContentHtml;
+    private String articleCreateTime;
+    private String articleState;
 }
